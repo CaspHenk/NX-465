6bcc6be5227fc8f976c6450a03f45cd3900e243a
Caspar Henking <caspar.henking@epfl.ch>
2025-04-26 22:12:36 +0200

    Up to 1.3 (not very clean but plots are here)

--- Project/utils.py
+++ Project/utils.py
@@ -71,3 +71,33 @@ def acc_next_state(S : np.ndarray, patterns : np.ndarray) :
     next_state = np.sign(h)
     
     return next_state
+
+
+# 1.1
+def overlap_next_state(S : np.ndarray, patterns : np.ndarray) :
+
+    ''' 
+    Exactly the same function as for 0.3, but here it also returns m
+    Not the most clever way to put it, but at least it separates the
+    two functions for the two exercises (we can rewrite it later if necessary)
+
+
+
+    Inputs :
+    S : np.ndarray of size (N,1) (state of the network at time i)
+    patterns : np.ndarray of size (p,N) (patterns that have to be compared to the state)
+
+    Output :
+    next_state : np.ndarray of size (N,1) (state of the network at time i+1)
+    m : np.ndarray of size (p,1) (overlap variables for each pattern)
+    '''
+    
+    p = patterns.shape[0]
+    N = patterns.shape[1]
+
+    # Compute overlap variables
+    m = 1/N * patterns @ S # (pxN) x (Nx1)
+    h = patterns.T @ m  # (Nxp) x (px1)
+    next_state = np.sign(h)
+    
+    return next_state, m
